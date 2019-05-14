5cd96fd8e63c37db63b200d7dd9b72decf754eb4
Five Grant <fivegrant@bennington.edu>
2019-05-14 00:43:00 +0000

    can read consistent data

--- data_in.py
+++ data_in.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python3
 from RPi.GPIO import *
+import time
 
 class Datalines:
-    def __init__(self, negative, positive, card_present):
+    def __init__(self, negative, positive, button):
         self.data0 = negative
         self.data1 = positive
-        self.present = card_present
+        self.button = button
         setmode(BOARD)
         setwarnings(False)
         setup(self.data0, IN)
         setup(self.data1, IN)
-        setup(self.present, IN)
-    def watch(self):
-        if input(self.present) == 0:
-            print("Note")
-    
+        setup(self.button, IN)
 
-Datalines
+    def watch(self, name):
+        start = time.time()
+        capture = ''
+        print(name)
+        print('')
+        while input(self.button) == 0:
+            if time.time() - start > 1:
+                start = time.time()
+                if capture != '':
+                    print(hex(int(int(capture, 2))))
+                    capture = ''
+            if input(self.data0) == LOW:
+                capture += '0'
+            elif input(self.data1) == LOW:
+                capture += '1'
+
+    def watch_improved(self, name):
+        start = time.time()
+        capture = ''
+        print(name)
+        print('')
+        while input(self.button) == 0:
+            if time.time() - start > 1:
+                start = time.time()
+                if capture != '':
+                    print(hex(int(int(capture, 2))))
+                    capture = ''
+            if input(self.data0) == LOW:
+                capture += '0'
+                #time.sleep(.00005)
+                time.sleep(.0000005)
+            elif input(self.data1) == LOW:
+                capture += '1'
+                time.sleep(.0000005)
+
+
+umm = Datalines(13,15,16)
+umm.watch_improved('Five')
  
