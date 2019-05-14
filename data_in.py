c5e23bdea51b4ed89c3a415809e58bfca2af7c45
Five Grant <fivegrant@bennington.edu>
2019-05-14 02:19:39 +0000

    easier testing to find proper value to read card. Honestly, im trying every possible combo

--- data_in.py
+++ data_in.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from RPi.GPIO import *
 import time
+import sys
 
 class Datalines:
     def __init__(self, negative, positive, button):
@@ -33,22 +34,43 @@ class Datalines:
         start = time.time()
         capture = ''
         switch = False
+        pos = (int(sys.argv[1]) - 1,int(sys.argv[2]))
+
         while input(self.button) == 0:
             if not switch and capture != '':
-                print("hexidecimal: " + hex(int(int(capture, 2))))
-                print("binary: " + bin(int(capture,2)))
-                print("decimal: " + str(int(capture,2)))
-                print("length: " + str(len(capture)))
+                """
+                done = False
+                for beg in range(0, len(capture) - 1):
+                    for end in range(beg, len(capture) - 1):
+                        pos = (beg, end)
+                        possible = capture[pos[0]:pos[1]]
+                        if len(possible) == 0:
+                            break
+                        if experiment == int(possible, 2):
+                            print("binary: " + capture[:pos[0]] + '|' + capture[pos[0]:pos[1]] + '|' + capture[pos[1]])
+                            print("Card Number: " + str(int(possible, 2)))
+                            print("length: " + str(len(possible)))
+                            capture = ''
+                            done = True
+                            break
+                    if done:
+                        break
+                        """
+                print("binary: " + capture[:pos[0]] + '|' + capture[pos[0]:pos[1]] + '|' + capture[:pos[1]])
+                print("Card Number: " + str(int(capture[pos[0]:pos[1]], 2)))
+                print("length: " + str(len(capture[pos[0]:pos[1]])))
                 capture = ''
+                done = True
+
             
             if input(self.data0) == LOW:
                 capture += '0'
-                time.sleep(.0000005)
+                time.sleep(.0005)
                 switch = True
                 start = time.time()
             elif input(self.data1) == LOW:
                 capture += '1'
-                time.sleep(.0000005)
+                time.sleep(.0005)
                 switch = True
                 start = time.time()
             elif time.time() - start > .5:
