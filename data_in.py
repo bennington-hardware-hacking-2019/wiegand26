4cd18c683072d07ead0d004d744b3336cad36865
Five Grant <fivegrant@bennington.edu>
2019-05-15 19:52:42 +0000

    it works. cut out a lot of bloat. all that's needed is the api.

--- data_in.py
+++ data_in.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from RPi.GPIO import *
 import time
-import sys
 
 class Datalines:
     def __init__(self, negative, positive, button):
@@ -14,78 +13,25 @@ class Datalines:
         setup(self.data1, IN)
         setup(self.button, IN)
 
-    def watch(self, name):
+    def watch(self):
         start = time.time()
         capture = ''
-        print(name)
-        print('')
-        while input(self.button) == 0:
-            if time.time() - start > 1:
-                start = time.time()
-                if capture != '':
-                    print(hex(int(int(capture, 2))))
-                    capture = ''
-            if input(self.data0) == LOW:
-                capture += '0'
-            elif input(self.data1) == LOW:
-                capture += '1'
-
-    def watch_improved(self):
-        start = time.time()
-        capture = ''
-        switch = False
-        pos = (int(sys.argv[1]) - 1,int(sys.argv[2]))
-
-        while input(self.button) == 0:
-            if not switch and capture != '':
-                """
-                capture = capture[::-1]
-                done = False
-                for beg in range(0, len(capture) - 1):
-                    for end in range(beg, len(capture) - 1):
-                        pos = (beg, end)
-                        possible = capture[pos[0]:pos[1]]
-                        if len(possible) == 0:
-                            break
-                        print(possible)
-                        if int(possible, 2) > 1000 and int(possible) < 5000:
-                            print("binary: " + capture[:pos[0]] + '|' + capture[pos[0]:pos[1]] + '|' + capture[pos[1]])
-                            print("Card Number: " + str(int(possible, 2)))
-                            print("length: " + str(len(possible)))
-                            capture = ''
-                            done = True
-                            break
-                    if done:
-                        break
-                """
-
-                print("binary: " + capture[:pos[0]] + '|' + capture[pos[0]:pos[1]] + '|' + capture[pos[1]:])
-                print("Card Number: " + str(int(capture[pos[0]:pos[1]], 2)))
-                """
-                print("\nHere's the reverse:")
-                capture = capture[::-1]
-                print("binary: " + capture[:pos[0]] + '|' + capture[pos[0]:pos[1]] + '|' + capture[pos[1]:])
-                print("Card Number: " + str(int(capture[pos[0]:pos[1]], 2)))
-                print('')
-                """
-                print(len(capture))
-                capture = ''
-                done = True
-
+        pos = (9,26)
+        while True:
             if input(self.data0) == LOW:
                 capture += '0'
                 time.sleep(float(sys.argv[3]))
-                switch = True
                 start = time.time()
             elif input(self.data1) == LOW:
                 capture += '1'
                 time.sleep(float(sys.argv[3]))
-                switch = True
                 start = time.time()
-            elif time.time() - start > .5:
-                switch = False
+            elif time.time() - start > .5 capture != '':
+                print("Card Number: " + str(int(capture[pos[0]:pos[1]], 2)))
+                break
 
 
-umm = Datalines(13, 15, 16)
-umm.watch_improved()
+
+umm = Datalines(15, 13, 16)
+umm.watch()
  
